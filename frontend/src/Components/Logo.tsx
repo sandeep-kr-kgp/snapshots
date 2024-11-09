98d1149ff655e1c13e6369ca83e111ab6cf194dd
sandeep-kr-kgp <sandeepkr.kgp@gmail.com>
2024-11-10 00:51:22 +0530

    Remove selection dropdown

--- frontend/src/Components/Logo.tsx
+++ frontend/src/Components/Logo.tsx
@@ -1,12 +1,11 @@
 import { Group, Text } from '@mantine/core';
-import SelectLocation from '../Sidebar/SelectLocation';
 function Logo() {
     return (
         <Group align="center">
             <Text fw={500} c="blue">
                 Snapshots
             </Text>
-            <SelectLocation />
+            {/* <SelectLocation /> */}
         </Group>
     );
 }
